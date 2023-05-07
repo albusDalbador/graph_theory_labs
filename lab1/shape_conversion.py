0a4f9df7ebbda568b64dad62c1c94c3d3450852f
albusDalbador <lemiasheuski@student.agh.edu.pl>
2023-05-07 12:58:33 +0200

    shape conversion updated

--- lab1/shape_conversion.py
+++ lab1/shape_conversion.py
@@ -1,39 +1,39 @@
 import numpy as np
+import networkx as nx
 
-
-def adj_list_to_adj_matrix(adj_list): # adj_list_to_adj_matrix
+def adj_list_to_adj_matrix(adj_list):
     # dla wygody i lepszej czytelności zapisujemy ilość węzłów w przetwarzanym grafie
     num_of_nodes = adj_list.shape[0]
     # inicjujemy macierz sąsiedstwa wypełnioną zerami
-    inc_matrix = np.zeros((num_of_nodes,num_of_nodes),dtype=np.int32)
+    adj_matrix = np.zeros((num_of_nodes,num_of_nodes),dtype=np.int32)
     # przechodzimy po liście sąsiedstwa
     for node,list_of_adjacents in enumerate(adj_list):
         # przechodzimy po wszystkih sąsiadach danego wierzchołka
         for adjacent in list_of_adjacents:
             # ustawiamy jedynki na pozycjach odpowiadacjącym krawędzi łączącej wierzchołki
-            inc_matrix[node,adjacent],inc_matrix[adjacent,node] = 1,1
-    return inc_matrix
+            adj_matrix[node,adjacent],adj_matrix[adjacent,node] = 1,1
+    return adj_matrix
 
 
-def adj_matrix_to_inc_matrix(inc_matrix): # naodwrót
+def adj_matrix_to_inc_matrix(adj_matrix): 
     # dla wygody i lepszej czytelności zapisujemy ilość węzłów w przetwarzanym grafie
-    num_of_nodes = inc_matrix.shape[0]
+    num_of_nodes = adj_matrix.shape[0]
     # macierz incydencji pierwotnie nie zawiera elementów
-    adj_matrix = []
+    inc_matrix = []
     # przechodzimy po wszystkich elementach w "górnym trójkącie" macierzy incydencji
     for i in range (num_of_nodes):
         for j in range (i,num_of_nodes -1):
             # napotkanie jedynki oznacza, że wierzchołek 'i' jest połączony z wierzchołkiem 'j'
-            if inc_matrix[i,j] == 1:
+            if adj_matrix[i,j] == 1:
                 # tworzymy nową krawędź i dodajemy ją do listy incydencji
                 step_row = [0]*num_of_nodes
                 step_row[i],step_row[j] = 1,1
-                adj_matrix.append(step_row)
+                inc_matrix.append(step_row)
     # tansponujemy macierz by uzyskać bardziej 'kanoniczną' postać, gdzie krawędzi są umieszczone pionowo
-    return np.transpose(adj_matrix)
+    return np.transpose(inc_matrix)
 
 
-def inc_matrix_to_adj_list(adj_matrix): # inc_matrix_to_adj_list
+def inc_matrix_to_adj_list(adj_matrix):
     # dla wygody i lepszej czytelności zapisujemy ilość węzłów w przetwarzanym grafie
     num_of_nodes = adj_matrix.shape[0]
     # inicjujemy listę sąsiedstwa 
@@ -58,3 +58,28 @@ def inc_matrix_to_adj_matrix(inc_matrix):
 
 def adj_matrix_to_adj_list(adj_matrix):
     return inc_matrix_to_adj_list(adj_matrix_to_inc_matrix(adj_matrix))
+
+
+# metody pomocowe
+def adj_matrix_to_nx_graph_object(adj_matrix):
+    adj_matrix = np.array(adj_matrix)
+    g = nx.Graph(directed=False)
+    num_of_nodes = adj_matrix.shape[0]
+
+    for ind in range(num_of_nodes):
+        g.add_node(ind)
+    
+    for i in range(num_of_nodes):
+        for j in range(i+1,num_of_nodes):
+            if adj_matrix[i,j] == 1:
+                g.add_edge(i,j)
+
+    return g
+
+
+def adj_list_to_nx_graph_object(adj_list):
+    return adj_matrix_to_nx_graph_object(adj_list_to_adj_matrix(adj_list))
+
+
+def inc_matrix_to_nx_graph_object(inc_matrix):
+    return adj_matrix_to_nx_graph_object(inc_matrix_to_adj_matrix(inc_matrix))
